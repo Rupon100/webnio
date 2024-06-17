77a53436323b0e3d0d4b0a0106e5843da24de6be
Rupoon <newaone@gmail.com>
2024-06-17 14:32:05 +0600

    add all file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        'bg-body': '#26355D',
+        'bg-box': ' #344C64',
+        'bg-opt': '#5A639C',
+      },
+    },
+  },
+  plugins: [],
+}
+
